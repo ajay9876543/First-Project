a2a4465f7b87a8dd2bcc16cc5ce09155fd578b07
ajay9876543 <ajay29052000@gmail.com>
2025-09-22 21:28:26 +0530

    Update FirstClass.java

--- TestProject/src/com/ajay/Demo/FirstClass.java
+++ TestProject/src/com/ajay/Demo/FirstClass.java
@@ -3,8 +3,7 @@ package com.ajay.Demo;
 public class FirstClass {
 	public static void main (String[] args) {
 		for(int i=0;i<5;i++) {
-			System.out.println("welcom");
+			System.out.println("welcom to git hub");
 		}
 	}
-
 }
